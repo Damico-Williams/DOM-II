51ec0811c15397d9fe1770c78282c78e3e586494
damico williams <damicowilliams@gmail.com>
2019-09-06 21:33:11 -0700

    first push

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+const funColor = document.getElementsByClassName('logo-heading')
+funColor.addEventListener('mouseover', function(event){
+   event.target.style.color = 'blue';
+});
