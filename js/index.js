60b1b9632011ba759ca4c6c7b5650d31083fd0d7
damico williams <damicowilliams@gmail.com>
2019-09-09 16:55:17 -0700

    most event listeners

--- js/index.js
+++ js/index.js
@@ -1,5 +1,62 @@
 // Your code goes here
 const funColor = document.getElementsByClassName('logo-heading')
-funColor.addEventListener('mouseover', function(event){
+funColor[0].addEventListener('mouseover', function(event){
    event.target.style.color = 'blue';
 });
+
+
+
+const btn = document.querySelectorAll('.destination .btn');
+btn[1].addEventListener('click', function(event) {
+   event.target.style.color = 'pink';    
+ });
+
+ const highLight = document.getElementsByTagName('h2')
+  highLight[0].addEventListener('select', function(event) {
+   alert('You highlighted a header!')    
+ });
+ 
+
+ const lg = document.querySelector('.text-content h2')
+ lg.addEventListener('dblclick', function(event) {
+   event.target.style.fontSize = '4rem';    
+ });
+
+
+ window.addEventListener('keydown', function(event) {
+    if(event.keyCode == 79){
+     event.target.style.backgroundColor = 'orange'
+    }
+ });
+
+const pTags = document.getElementsByTagName('p')
+ window.addEventListener('resize', function(event) {
+    pTags[0].textContent = 'hahahaha!'
+    
+});
+
+const ghost = document.querySelector('.nav')
+ ghost.addEventListener('copy', function(event) {
+   event.target.style.display = 'none';    
+ });
+
+ window.addEventListener('keyup', function(event) {
+   if(event.keyCode == 66){
+    event.target.style.width = '50rem'
+   }
+});
+
+const invis = document.querySelector('.intro img')
+ invis.addEventListener('contextmenu', function(event) {
+   event.target.style.display = 'none';    
+ });
+
+//  btn[0].addEventListener('focus', function(event) {
+//    event.target.style.background = 'pink';    
+//  },true);
+ 
+//  btn[0].addEventListener('blur', function(event) {
+//    event.target.style.background = 'red';    
+//  },true);
+
+
